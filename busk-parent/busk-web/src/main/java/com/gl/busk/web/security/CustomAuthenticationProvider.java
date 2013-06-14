ba81a3750c96ae615459fad378815910988218bf
gabriel <eldalai@gmail.com>
2013-06-13 22:03:50 -0300

    fix

--- busk-parent/busk-web/src/main/java/com/gl/busk/web/security/CustomAuthenticationProvider.java
+++ busk-parent/busk-web/src/main/java/com/gl/busk/web/security/CustomAuthenticationProvider.java
@@ -1,17 +1,8 @@
 package com.gl.busk.web.security;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.jruby.compiler.ir.operands.Array;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.GrantedAuthorityImpl;
 
 public class CustomAuthenticationProvider implements AuthenticationProvider {
 
